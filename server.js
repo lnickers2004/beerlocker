81f254a73b82f94e38bd106b40fb9e9eec6707bd
Larry Nickerson <lnickers@austin.rr.com>
2015-03-19 04:53:25 -0500

    added passport basic authentication to routes

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
+var beerController = require('./controllers/beer');
+var userController = require('./controllers/user');
+var passport = require('passport');
+var authController = require('./controllers/auth');
 
 // Connect to the beerlocker MongoDB
 mongoose.connect('mongodb://localhost:27017/beerlocker');
@@ -15,110 +18,30 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Use environment defined port or 3000
-var port = process.env.PORT || 3000;
+// Use the passport package in our application
+app.use(passport.initialize());
 
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-    res.json({ message: 'You are running dangerously low on beer!' });
-});
+// Create endpoint handlers for /beers
+router.route('/beers')
+    .post(authController.isAuthenticated, beerController.postBeers)
+    .get(authController.isAuthenticated, beerController.getBeers);
 
-// -- New Code Below Here -- //
+// Create endpoint handlers for /beers/:beer_id
+router.route('/beers/:beer_id')
+    .get(authController.isAuthenticated, beerController.getBeer)
+    .put(authController.isAuthenticated, beerController.putBeer)
+    .delete(authController.isAuthenticated, beerController.deleteBeer);
 
-// Create a new route with the prefix /beers
-var beersRoute = router.route('/beers');
-
-// Create endpoint /api/beers for POSTS
-beersRoute.post(function(req, res) {
-    // Create a new instance of the Beer model
-    var beer = new Beer();
-
-    // Set the beer properties that came from the POST data
-    beer.name = req.body.name;
-    beer.type = req.body.type;
-    beer.quantity = req.body.quantity;
-
-    // Save the beer and check for errors
-    beer.save(function(err) {
-        if (err)
-            res.send(err);
-
-        res.json({ message: 'Beer added to the locker!', data: beer });
-    });
-});
-
-// -- New Code Below Here -- //
-
-// Create endpoint /api/beers for GET
-beersRoute.get(function(req, res) {
-    // Use the Beer model to find all beer
-    Beer.find(function(err, beers) {
-        if (err)
-            res.send(err);
-
-        res.json(beers);
-    });
-});
-
-// -- New Code Below Here -- //
-
-// Create a new route with the /beers/:beer_id prefix
-var beerRoute = router.route('/beers/:beer_id');
-
-// Create endpoint /api/beers/:beer_id for GET
-beerRoute.get(function(req, res) {
-    // Use the Beer model to find a specific beer
-    Beer.findById(req.params.beer_id, function(err, beer) {
-        if (err)
-            res.send(err);
-
-        res.json(beer);
-    });
-});
-
-// -- New Code Below Here -- //
-
-// Create endpoint /api/beers/:beer_id for PUT
-beerRoute.put(function(req, res) {
-    // Use the Beer model to find a specific beer
-    Beer.findById(req.params.beer_id, function(err, beer) {
-        if (err)
-            res.send(err);
-
-        // Update the existing beer quantity
-        beer.quantity = req.body.quantity;
-
-        // Save the beer and check for errors
-        beer.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json(beer);
-        });
-    });
-});
-
-// -- New Code Below Here -- //
-
-// Create endpoint /api/beers/:beer_id for DELETE
-beerRoute.delete(function(req, res) {
-    // Use the Beer model to find a specific beer and remove it
-    Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-        if (err)
-            res.send(err);
-
-        res.json({ message: 'Beer removed from the locker!' });
-    });
-});
-
+// Create endpoint handlers for /users
+router.route('/users')
+    .post(userController.postUsers)
+    .get(authController.isAuthenticated, userController.getUsers);
 
 // Register all our routes with /api
 app.use('/api', router);
 
 // Start the server
-app.listen(port);
-console.log('Insert beer on port ' + port);
+app.listen(3000);
